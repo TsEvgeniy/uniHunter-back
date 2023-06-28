e1fe4b7195a37543317a3682cac94f5f44b2b9a1
TsEvgeniy <evgeniy.tsvetikov@icloud.com>
2023-06-29 02:29:40 +0500

    registration, auth

---
+++ middlewares/authMiddlewares.js
@@ -0,0 +1,43 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+const requireAuth = (req, res, next) => {
+    const token = req.cookies.jwt;
+
+    if(token) {
+        jwt.verify(token, 'secret', (err, decodedToken) => {
+            if (err) {
+                res.send({message: "wrong"});
+            } else {
+                next();
+            }
+        })
+    } else { 
+        res.send({message: "wrong"});
+    }
+};
+
+const checkUser = (req, res, next) => {
+    const token = req.cookies.jwt;
+
+    if (token) {
+        jwt.verify(token, 'secret', async (err, decodedToken) => {
+            if (err) {
+                res.locals.user = null;
+                next();
+            } else {
+                let user = await User.findById(decodedToken.id);
+                res.locals.user = user;
+                next();
+            }
+        })
+    } else {
+        res.locals.user = null;
+        next();
+    }    
+}
+
+module.exports = {
+    requireAuth,
+    checkUser
+}
